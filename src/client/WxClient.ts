df6abf49c314469aba3a3d79fabb62a67712b9bb
xleat <xleat@outlook.com>
2025-04-06 23:40:16 +0800

    v0.4.5 无法启动

--- src/client/WxClient.ts
+++ src/client/WxClient.ts
@@ -19,6 +19,7 @@ import {quote} from "../util/GewePostUtils";
 export class WxClient extends AbstractClient<GeweBot> {
 
     private scanPhotoMsgId: number[] = []
+    private messageSet: Set<string> = new Set();
     private readonly wxMessageHelper = WxMessageHelper.getInstance(WxMessageHelper);
     private readonly prismaService = PrismaService.getInstance(PrismaService)
 
@@ -64,7 +65,7 @@ export class WxClient extends AbstractClient<GeweBot> {
         return new Promise<boolean>((resolve, reject) => {
 
             if (this.hasLogin) {
-                return reject(new Error("Login failed."));
+                return reject('已经登录')
             }
 
 
@@ -151,7 +152,7 @@ export class WxClient extends AbstractClient<GeweBot> {
                 resolve(true)
 
             }).catch(e => {
-                reject(new Error(e))
+                reject(e)
             })
 
             this.onMessage(null)
@@ -191,7 +192,7 @@ export class WxClient extends AbstractClient<GeweBot> {
                                 case 'video': // 视频使用文件类型
                                 case "file":
                                 case "audio":
-                                case "image": {
+                                case "image":
                                     const forceType = msgParams.msgType === 'image' ? 'image' : 'file'
                                     let file = msgParams.file as string;
                                     if (!file.startsWith('http')) {
@@ -199,7 +200,6 @@ export class WxClient extends AbstractClient<GeweBot> {
                                     }
                                     const fileBox = Filebox.fromUrl(file, forceType)
                                     to.say(fileBox).then(resolve).catch(reject)
-                                }
                                     break;
                                 case "quote":
                                     this.prismaService.prisma.message.findFirst({
@@ -248,6 +248,29 @@ export class WxClient extends AbstractClient<GeweBot> {
         return this.bot.checkOnline()
     }
 
+    private async isDuplicateMessage(msgId: string): Promise<boolean> {
+        const message = await this.prismaService.prisma.message.findFirst({
+            where: {id: Number(msgId)},
+        });
+        if (this.messageSet.has(msgId) || message) {
+            return true;
+        }
+        this.messageSet.add(msgId);
+        this.prismaService.prisma.wx_msg_filter.upsert({
+            where: {
+                id: msgId
+            },
+            update: {
+                id: msgId
+            },
+            create: {
+                id: msgId
+            }
+        }).then()
+        setTimeout(() => this.messageSet.delete(msgId), 60000);
+        return false;
+    }
+
     onMessage(any: any): void {
         this.bot.on('scan', qrcode => {
             if (qrcode) {
@@ -271,10 +294,9 @@ export class WxClient extends AbstractClient<GeweBot> {
         })
         this.bot.on('message', async (msg) => {
 
-                if (await this.wxMessageHelper.isDuplicateMessage(msg._newMsgId + '')) {
+                if (this.isDuplicateMessage(msg._newMsgId)) {
                     return
                 }
-
                 // 只处理登录之后的消息 且不是发送给文件助手的消息
                 // 没有类型的消息不处理，大多是通知或者无法处理的消息
                 if (msg._createTime >= this.loginTime
@@ -287,10 +309,9 @@ export class WxClient extends AbstractClient<GeweBot> {
         this.bot.on('room-invite', async (roomInvitation) => {
 
         })
-        this.bot.on('friendship', async (friendship) => {
-
+        this.bot.on('friendship', (friendship) => {
             // @ts-ignore
-            if (await this.wxMessageHelper.isDuplicateMessage(friendship.fromId + '')) {
+            if (this.isDuplicateMessage(friendship.fromId)) {
                 return
             }
 
@@ -321,7 +342,15 @@ export class WxClient extends AbstractClient<GeweBot> {
         })
         // 撤回消息
         this.bot.on('revoke', async msg => {
-            if (await this.wxMessageHelper.isDuplicateMessage(msg._newMsgId)) {
+            // this.logDebug('wx revoke', msg)
+            // 这个也有问题会被触发两次
+            if (this.isDuplicateMessage(msg._newMsgId)) {
+                return
+            }
+            const existMsg = await this.prismaService.prisma.wx_msg_filter.findUnique({
+                where: {id: msg._newMsgId.toString()},
+            })
+            if (existMsg) {
                 return
             }
             parseSysMsgPayload(msg.text()).then(sysMsgPayload => {
